b38e00e6524152c8fe98b8c3c50d0e2318782be4
StellaWanja <stylades@gmail.com>
2024-04-06 12:28:46 +0300

    Added validation for the sign up route

--- backend/src/database/schema.sql
+++ backend/src/database/schema.sql
@@ -3,12 +3,15 @@ USE curioscribe;
 
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    confirm_password VARCHAR(255) NOT NULL,
     created DATETIME NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO users (username, email, password)
-VALUES  ("user1", "user1@test.com", "123456" ),
-        ("user2", "user2@test.com", "123456");
+INSERT INTO users (first_name, last_name, username, email, password, confirm_password)
+VALUES  ("john", "doe", "user1", "user1@test.com", "123456", "123456"),
+        ("jane", "doe", "user2", "user2@test.com", "123456", "123456");
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,23 +1,77 @@
 import express, { Request, Response } from "express";
-import { register } from "../controllers/userController.js";
+import bcrypt from "bcrypt";
 import initializeDB from "../database/database.js";
 
 const pool = await initializeDB();
-
-
 const routes = express.Router();
 
-routes.post('/register', async(req: Request, res: Response) => {
-    try {
-        const { username, email, password } = req.body;
-        const connection = await pool.getConnection();
-        const [result] = await connection.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password]);
-        connection.release();
-        res.status(200).send('Signed up successfully');
-    } catch (error) {
-        console.error('Error signing up:', error);
-        res.status(500).send('Error signing up');
+const emailRegex = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9]+\.[A-Za-z]/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[#@$!%*?&])[A-Za-z\d#@$!%*?&]{8,}$/;
+
+// signup route
+routes.post("/signup", async (req: Request, res: Response) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      username,
+      email,
+      password,
+      confirm_password,
+    } = req.body;
+
+    /* VALIDATION */
+    // null values
+    if (
+      !first_name ||
+      !last_name ||
+      !username ||
+      !email ||
+      !password ||
+      !confirm_password
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Please fill in the required fields", status: 400 });
     }
+
+    // check if email is valid
+    if (!emailRegex.test(email)) {
+      return res
+        .status(400)
+        .json({ message: "Please enter a valid email address", status: 400 });
+    }
+
+    // check if password is valid
+    if (!passwordRegex.test(password)) {
+        return res
+          .status(400)
+          .json({ message: "Password must be at least 8 characters long with at least 1 digit, a special character, an uppercase letter, and a lowercase letter", status: 400 });
+    }
+
+    // check if passwords match
+    if (password !== confirm_password) {
+        return res
+          .status(400)
+          .json({ message: "Passwords entered do not match", status: 400 });
+    }
+
+    // hash password
+
+
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute(
+      "INSERT INTO users (first_name, last_name, username, email, password, confirm_password) VALUES (?, ?, ?, ?, ?, ?)",
+      [first_name, last_name, username, email, password, confirm_password]
+    );
+    connection.release();
+    res.status(200).json({ message: "Registration successful!", status: 200 });
+  } catch (error) {
+    console.error("Error signing up:", error);
+    res
+      .status(500)
+      .json({ message: `Error signing up: ${error}`, status: 500 });
+  }
 });
 
 export default routes;
